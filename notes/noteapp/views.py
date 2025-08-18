69168370dfb900ce16b2187a086a88f0e5bd0dc9
Oleksii <user@example.com>
2025-08-18 23:41:53 +0300

    1

---
+++ notes/noteapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+def main(request):
+    return render(request, 'noteapp/index.html')
+
+def tag(request):
+    if request.method == 'POST':
+        form = TagForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='noteapp:main')
+        else:
+            return render(request, 'noteapp/tag.html', {'form': form})
+
+    return render(request, 'noteapp/tag.html', {'form': TagForm()})
